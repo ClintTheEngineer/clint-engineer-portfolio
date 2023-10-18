0c5780e87420ce3a18890ab798073ce38feece5a
ClintTheEngineer <81938100+ClintTheEngineer@users.noreply.github.com>
2023-10-17 20:52:27 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Projects from './components/Projects';
 import TechStack from './components/TechStack';
 import Contact from './components/Contact';
 import FloatingButton from './components/FloatingButton';
-import AnimatedTextSlide from './components/AnimatedTextSlide';
+
 
 function App() {
+  
   return (
     <div className="App">
      <LandingPage />
-     <AnimatedTextSlide text="Purple!" />
      <Projects />
      <TechStack />
      <Contact/>
